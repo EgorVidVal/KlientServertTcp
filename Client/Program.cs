c2b0f44dd0af73354e278b2a02e636cfe35217de
Apokadr@outlook.com <Apokadr@outlook.com>
2020-01-28 17:05:04 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 7000);
+                Console.WriteLine("Клиент подключился");
+
+
+                NetworkStream stream = client.GetStream();
+
+               
+                //реализует постоянную отпраку сообщений без разрыва
+                while(true)
+                {
+                    Console.ReadKey();
+                    string message = "сообщения от клиента";
+                    byte[] byteWrite = Encoding.UTF8.GetBytes(message);
+                    //какой массив отправляем, от скольки, до скольки.
+                    stream.Write(byteWrite, 0, byteWrite.Length);
+                    stream.Flush();
+                }
+                   
+
+                    byte[] bytesRead = new byte[256];
+
+                    stream.Read(bytesRead, 0, bytesRead.Length);
+                    string answer = Encoding.UTF8.GetString(bytesRead);
+                    Console.Write(answer);
+
+
+
+                    
+                
+                client.Close();
+
+
+                Console.WriteLine("клиент отключился");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KlientServert/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlientServert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //через какой порт стучаться к нам
+                //PAddress.Any - получить запрос откуда угодно.
+                TcpListener serverSoket = new TcpListener(IPAddress.Any, 7000);
+                Console.WriteLine("Старт сервера");
+                serverSoket.Start();
+                
+                while(true)
+                {
+
+                   
+
+                    //ставим сервер на ожидание.
+                    TcpClient clientSoket = serverSoket.AcceptTcpClient();
+                    //чтобы отвечать клиенту
+                    NetworkStream stream = clientSoket.GetStream();
+
+                    //постоянно слушает соединения.
+                    while(true)
+                    {
+                        byte[] bytes = new byte[256];
+                        stream.Read(bytes, 0, bytes.Length);
+                        string request = Encoding.UTF8.GetString(bytes);
+                        Console.WriteLine("ответ \n" + request);
+
+                        string message = "Длина сообщения" + request.Length;
+                        bytes = Encoding.UTF8.GetBytes(message);
+                        //какой массив отправляем, от скольки, до скольки.
+                        stream.Write(bytes, 0, bytes.Length);
+                        stream.Flush();
+                    }
+                    
+
+                   // clientSoket.Close();
+
+                }
+
+
+                
+                serverSoket.Stop();
+
+                Console.WriteLine("Сервер стоп");
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
